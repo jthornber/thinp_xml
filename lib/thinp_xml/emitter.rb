d860c1d8bd6f1efbf51c64990ba6bd75d7ee0e37
Joe Thornber <ejt@redhat.com>
2013-08-21 11:48:54 +0100

    Factor out thinp_xml/emitter.rb

---
+++ lib/thinp_xml/emitter.rb
@@ -0,0 +1,40 @@
+#----------------------------------------------------------------
+
+module ThinpXML
+  module Base
+    class Emitter
+      def initialize(out)
+        @out = out
+        @indent = 0
+      end
+
+      def emit_tag(obj, tag, *fields, &block)
+        expanded = fields.map {|fld| "#{fld}=\"#{obj.send(fld)}\""}
+        if block.nil?
+          emit_line "<#{tag} #{expanded.join(' ')}/>"
+        else
+          emit_line "<#{tag} #{expanded.join(' ')}>"
+          push
+          yield unless block.nil?
+          pop
+          emit_line "</#{tag}>"
+        end
+      end
+
+      private
+      def emit_line(str)
+        @out.puts((' ' * @indent) + str)
+      end
+
+      def push
+        @indent += 2
+      end
+
+      def pop
+        @indent -= 2
+      end
+    end
+  end
+end
+
+#----------------------------------------------------------------
--- lib/thinp_xml/thinp/emit.rb
+++ lib/thinp_xml/thinp/emit.rb
@@ -1,64 +1,43 @@
 require 'thinp_xml/thinp/metadata'
+require 'thinp_xml/emitter'
 
 #----------------------------------------------------------------
 
 module ThinpXML
-  class Emitter
-    def initialize(out)
-      @out = out
-      @indent = 0
-    end
+  module EmitterDetail
+    class ThinpEmitter
+      def initialize(out)
+        @e = ThinpXML::Base::Emitter.new(out)
+      end
 
-    def emit_tag(obj, tag, *fields, &block)
-      expanded = fields.map {|fld| "#{fld}=\"#{obj.send(fld)}\""}
-      if block.nil?
-        emit_line "<#{tag} #{expanded.join(' ')}/>"
-      else
-        emit_line "<#{tag} #{expanded.join(' ')}>"
-        push
-        yield unless block.nil?
-        pop
-        emit_line "</#{tag}>"
+      def emit_superblock(sb, &block)
+        @e.emit_tag(sb, 'superblock', :uuid, :time, :transaction, :data_block_size, :nr_data_blocks, &block)
+      end
+
+      def emit_device(dev, &block)
+        @e.emit_tag(dev, 'device', :dev_id, :mapped_blocks, :transaction, :creation_time, :snap_time, &block)
+      end
+
+      def emit_mapping(m)
+        if m.length == 1
+          @e.emit_line("<single_mapping origin_block=\"#{m.origin_begin}\" data_block=\"#{m.data_begin}\" time=\"#{m.time}\"/>")
+        else
+          @e.emit_tag(m, 'range_mapping', :origin_begin, :data_begin, :length, :time)
+        end
       end
     end
-
-    def emit_line(str)
-      @out.puts((' ' * @indent) + str)
-    end
-
-    def push
-      @indent += 2
-    end
-
-    def pop
-      @indent -= 2
-    end
   end
 
-  def emit_superblock(e, sb, &block)
-    e.emit_tag(sb, 'superblock', :uuid, :time, :transaction, :data_block_size, :nr_data_blocks, &block)
-  end
-
-  def emit_device(e, dev, &block)
-    e.emit_tag(dev, 'device', :dev_id, :mapped_blocks, :transaction, :creation_time, :snap_time, &block)
-  end
-
-  def emit_mapping(e, m)
-    if m.length == 1
-      e.emit_line("<single_mapping origin_block=\"#{m.origin_begin}\" data_block=\"#{m.data_begin}\" time=\"#{m.time}\"/>")
-    else
-      e.emit_tag(m, 'range_mapping', :origin_begin, :data_begin, :length, :time)
-    end
-  end
+  #--------------------------------
 
   def write_xml(metadata, io)
-    e = Emitter.new(io)
+    e = EmitterDetail::ThinpEmitter.new(io)
 
-    emit_superblock(e, metadata.superblock) do
+    e.emit_superblock(metadata.superblock) do
       metadata.devices.each do |dev|
-        emit_device(e, dev) do
+        e.emit_device(dev) do
           dev.mappings.each do |m|
-            emit_mapping(e, m)
+            e.emit_mapping(m)
           end
         end
       end
